fbadf0b5f9c118553e7948523787205d05124f3e
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-12-25 10:45:58 +0700

    feat(dashboard): redirect admin users from dashboard

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -1,12 +1,17 @@
 import { AppSidebar } from "../dashboard/AppSidebar";
 import DashboardHeader from "../dashboard/DashboardHeader";
 import { SidebarProvider } from "../ui/sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Toaster } from "../ui/sonner";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { useSelector } from "react-redux";
 
 export default function DashboardLayout() {
   const isMobile = useIsMobile();
+  const navigate = useNavigate();
+  const isAdmin = useSelector((state) => state.auth.role) === "admin";
+
+  if (isAdmin) return navigate("/admin");
   return (
     <div className="bg-foreground/2">
       <SidebarProvider>
